4b3954385677f9e46c0aeca6705a376eb9c4fec6
Samaya17 <mughumova@gmail.com>
2022-10-06 17:00:30 +0400

    commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+/*---- Preloader JS ---- */
+
+setTimeout(function () {
+    $('.loader_bg').fadeToggle();
+}, 2000);
+
+/*---- Cursor JS ----*/
+
+// const cursor = document.querySelector('.cursor-inner');
+// const cursor2 = document.querySelector('.cursor-outer');
+// document.addEventListener('mousemove', (e) => {
+
+//     cursor.style.left = e.pageX + 'px';
+//     cursor.style.top = e.pageY + 'px';
+
+//     cursor2.style.left = e.pageX + 'px';
+//     cursor2.style.top = e.pageY + 'px';
+
+// });
+
